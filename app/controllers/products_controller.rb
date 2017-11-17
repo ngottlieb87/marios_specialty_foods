9ee6888f2001debba0f2b8d3b1fbf750e0b03a59
ng <nogpdx@gmail.com>
2017-11-17 13:20:21 -0800

    add edit and delete for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,26 @@ class ProductsController < ApplicationController
     render :show
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :cost, :country_origin)
